a5344d367cdebaa063048a1a779ce62c5d0f6118
Pranavireddy30 <jaidipranavireddy@gmail.com>
2026-06-21 23:22:44 +0530

    Implemented winner detection

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ void initializeBoard() {
     }
 }
 
-void DisplayBoard() {
+void displayBoard() {
     std::cout << "\n";
     for(int row = 0; row < 3; row++) {
         std::cout << " ";
@@ -42,13 +42,39 @@ void playerTurn(char symbol) {
         playerTurn(symbol);
     }
 }
-
+bool checkWinner(char symbol) {
+    // Check rows
+    for(int row = 0; row < 3; row++) {
+        if(board[row][0] == symbol && board[row][1] == symbol && board[row][2] == symbol) {
+            return true;
+        }
+    }
+    // Check columns
+    for(int col = 0; col < 3; col++) {
+        if(board[0][col] == symbol && board[1][col] == symbol && board[2][col] == symbol) {
+            return true;
+        }
+    }
+    // Check diagonals
+    if(board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
+        return true;
+    }
+    if(board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
+        return true;
+    }
+    return false;
+}
 int main() {
     initializeBoard();
     char currentPlayer = 'X';
     while(true) {
-        DisplayBoard();
+        displayBoard();
         playerTurn(currentPlayer);
+        if(checkWinner(currentPlayer)) {
+            displayBoard();
+            std::cout << "Player "<< currentPlayer << " wins!\n";
+            break;
+        }
         currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
     }
     return 0;
