90099dc6430a4e23bbfe3d262213e78af31b933e
Pranavireddy30 <jaidipranavireddy@gmail.com>
2026-06-21 15:08:37 +0530

    Add player input

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
 int main() {
+    int position;
+
     std::cout << "\n";
-    std::cout << "---------------------\n";
+    std::cout << "-------------\n";
     std::cout << "| 1 | 2 | 3 |\n";
-    std::cout << "---------------------\n";
+    std::cout << "-------------\n";
     std::cout << "| 4 | 5 | 6 |\n";
-    std::cout << "---------------------\n";
+    std::cout << "-------------\n";
     std::cout << "| 7 | 8 | 9 |\n";
-    std::cout << "---------------------\n";
-    
+    std::cout << "-------------\n";
+    std::cout << "Choose the position (1-9): ";
+    std::cin>> position;
+    std::cout << "You have chosen position: " << position << std::endl;
     return 0;
 }
